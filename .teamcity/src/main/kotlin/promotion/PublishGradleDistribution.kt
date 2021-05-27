a76b2c35fbf4ddbadc7417e1d3c0462f4eeeae98
Bo Zhang <bo@gradle.com>
2021-05-27 16:21:21 +0800

    Fix parameter

--- .teamcity/src/main/kotlin/promotion/PublishGradleDistribution.kt
+++ .teamcity/src/main/kotlin/promotion/PublishGradleDistribution.kt
@@ -47,7 +47,7 @@ abstract class PublishGradleDistribution(
             gradleWrapper {
                 name = "Promote"
                 tasks = task
-                """-PcommitId=%Gradle_${promotedBranch.capitalize()}_Check_Stage_${this@PublishGradleDistribution.triggerName}_Trigger.build.vcs.number% $extraParameters "-PgitUserName=$gitUserName" "-PgitUserEmail=$gitUserEmail"  """.also { gradleParams = it }
+                """-PcommitId=%dep.Gradle_${promotedBranch.capitalize()}_Check_Stage_${this@PublishGradleDistribution.triggerName}_Trigger.build.vcs.number% $extraParameters "-PgitUserName=$gitUserName" "-PgitUserEmail=$gitUserEmail"  """.also { gradleParams = it }
             }
         }
         /**
