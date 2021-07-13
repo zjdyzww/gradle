0ab70f4e1c54297b9efe5db3bd115ef1b1681a11
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2021-07-13 14:35:38 -0300

    Upgrade embedded Kotlin version to 1.5.21

--- build-logic/dependency-modules/src/main/kotlin/gradlebuild/modules/extension/ExternalModulesExtension.kt
+++ build-logic/dependency-modules/src/main/kotlin/gradlebuild/modules/extension/ExternalModulesExtension.kt
@@ -21,7 +21,7 @@ import gradlebuild.modules.model.License
 abstract class ExternalModulesExtension {
 
     val groovyVersion = "3.0.7"
-    val kotlinVersion = "1.5.20"
+    val kotlinVersion = "1.5.21"
 
     fun futureKotlin(module: String) = "org.jetbrains.kotlin:kotlin-$module:$kotlinVersion"
 
